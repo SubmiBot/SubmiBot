f97ae49e29919099885dbfe51cc2849026a1b45f
Gabriel Fernandes <gabrielfernnd@gmail.com>
2018-07-04 11:16:58 -0300

    um ola inicial

--- src/p2/submibot/SubmiBot.java
+++ src/p2/submibot/SubmiBot.java
@@ -1,3 +1,23 @@
 package p2.submibot;
 
-public class SubmiBot {}
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+public class SubmiBot extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+
+		MessageDialog.openInformation(
+				window.getShell(),
+				"SubmiBot title",
+				"Welcome to SubmiBot");
+		return null;
+	}
+
+}
