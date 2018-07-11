a69fcfb93d355d9b0445d1598ea434a0f1f0cd37
Héricles Emanuel <hericles.me@gmail.com>
2018-07-11 13:50:53 -0300

    Unstable zip with dialog

--- src/p2/submibot/handlers/SubmitProject.java
+++ src/p2/submibot/handlers/SubmitProject.java
@@ -1,16 +1,20 @@
 package p2.submibot.handlers;
 
+import java.io.IOException;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 import p2.submibot.ui.CredentialsHandler;
+import p2.submibot.util.Zip;
 
 public class SubmitProject extends AbstractHandler {
 
@@ -23,8 +27,24 @@ public class SubmitProject extends AbstractHandler {
 			if (firstElement instanceof IAdaptable) {
 				IProject project = (IProject) ((IAdaptable) firstElement).getAdapter(IProject.class);
 				if (project != null) {
-					CredentialsHandler handler = new CredentialsHandler(window.getShell());
-					handler.execute();
+					try {
+						CredentialsHandler handler = new CredentialsHandler(window.getShell());
+						handler.execute();
+						
+						while(handler.getState()) continue;
+						
+						try {
+
+							IPath path = project.getLocation();
+
+							Zip.zip(path.toOSString(), path.toOSString() + IPath.SEPARATOR + handler.getFilename() + ".zip");
+							MessageDialog.openInformation(window.getShell(), "Zip criado com sucesso", "Zip do projeto em " + path.toOSString());
+						} catch (IOException e) {
+							MessageDialog.openInformation(window.getShell(), "Erro", "Não foi possível criar o zip do projeto");
+						}
+					} catch (Exception e) {
+						MessageDialog.openError(window.getShell(), "Erro", "Não foi possível efetuar a submissão");
+					}
 				} else {
 					MessageDialog.openError(window.getShell(), "Erro", "Clique sobre um projeto");
 				}
@@ -32,5 +52,4 @@ public class SubmitProject extends AbstractHandler {
 		}
 		return null;
 	}
-
 }
