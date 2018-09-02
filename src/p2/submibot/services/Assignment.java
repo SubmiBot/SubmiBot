cdc28107cd1dab54904971dd510ebdde6b56d2d0
Gabriel Fernandes <gabrielfernnd@gmail.com>
2018-09-01 21:08:22 -0300

    mostrando lista de assignments do aluno

---
+++ src/p2/submibot/services/Assignment.java
@@ -0,0 +1,12 @@
+package p2.submibot.services;
+
+public class Assignment {
+
+	String id;
+	String name;
+	
+	@Override
+	public String toString() {
+		return "id: " + this.id + "\nname: " + this.name + '\n';
+	}
+}
--- src/p2/submibot/services/InfoService.java
+++ src/p2/submibot/services/InfoService.java
@@ -5,7 +5,6 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import com.google.gson.Gson;
@@ -24,7 +23,7 @@ public class InfoService {
 		con.setRequestProperty("Authorization", token);
 		con.setRequestMethod("GET");
 
-		int responseCode = con.getResponseCode();
+		// int responseCode = con.getResponseCode();
 
 		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
 		String inputLine;
@@ -42,9 +41,40 @@ public class InfoService {
 		System.out.println(user);
 	}
 
+	private void getAssignments(String token) throws Exception {
+
+		String url = "https://canvas.instructure.com/api/v1/courses/1374512/assignments";
+
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		// optional default is GET
+		con.setRequestProperty("Authorization", token);
+		con.setRequestMethod("GET");
+
+		// int responseCode = con.getResponseCode();
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		Gson gson = new Gson();
+		@SuppressWarnings("unchecked")
+		List<Assignment> assigs = (ArrayList<Assignment>) gson.fromJson(response.toString(), new TypeToken<ArrayList<Assignment>>(){}.getType());
+
+		// print result
+		System.out.println(assigs);
+	}
+	
 	public static void main(String[] args) throws Exception {
 		InfoService is = new InfoService();
 		is.getUser("Bearer 7~P8NctVMFwiLKZeP73EiRzObXGpfMlPQLiDBqmmAd7Lhojzm6ylYU4As0hW9GEkAQ");
+		is.getAssignments("Bearer 7~P8NctVMFwiLKZeP73EiRzObXGpfMlPQLiDBqmmAd7Lhojzm6ylYU4As0hW9GEkAQ");
 	}
 
 }
